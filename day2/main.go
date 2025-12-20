363d47726cdfd5db9b0b2bf6a18aa05a5a5c7e1d
0xbea <19748276+0xbea@users.noreply.github.com>
2025-12-20 23:32:59 +0100

    use set

--- day2/main.go
+++ day2/main.go
@@ -18,7 +18,7 @@ func main() {
 	defer file.Close()
 
 	// set initial / global variables
-	var invalidList []int // list of invalid IDs
+	var invalidList []map[int]bool
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
@@ -39,8 +39,10 @@ func main() {
 			// todo: dont bother with range where both have odd digits and same number
 			// invalid ID:  any ID which is made only of some sequence of digits repeated twice.
 			// generate invalid IDs for a range
+			// each range has a set
+			s := map[int]bool{}
 			for i := start; i <= end; i++ {
-				// each range has a set
+
 				// dont bother with IDs with odd number digits
 				numDigits := len(strconv.Itoa(i))
 				if numDigits%2 != 0 {
@@ -54,8 +56,9 @@ func main() {
 				if checkId > end {
 					break
 				}
-				invalidList = append(invalidList, checkId)
+				s[checkId] = true
 			}
+			invalidList = append(invalidList, s)
 		}
 
 	}
