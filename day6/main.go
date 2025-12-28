5b70248e44db5be79384b5e743c57453981d8729
0xbea <19748276+0xbea@users.noreply.github.com>
2025-12-28 17:47:09 +0100

    pseudocode

--- day6/main.go
+++ day6/main.go
@@ -15,11 +15,14 @@ func main() {
 	defer file.Close()
 
 	// set initial / global variables
+	// create slice of int slices
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
 		// process each line
+		// add ints to slice
+		// if symbol, do operation for slice
 
 	}
 
